3b3f57515eb66d5295ac4aab13c1622bf6c11248
Fodé CISSE <contact+fode@zotomatise.com>
2026-07-22 20:17:32 +0200

    config cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
+  projectId: "heunuv",
+
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
